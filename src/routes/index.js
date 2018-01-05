8a17110e11e86871e1a3512eef222a3509e4df18
CTzz <ctzz@protonmail.com>
2018-01-05 14:25:05 +0800

    Clean Format

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,6 @@ const apiRouter = require('./api/index');
 const router = new Router();
 
 router.use('/auth', authRouter.routes(), authRouter.allowedMethods());
-router.use('/api', passport.authenticate('jwt', { session: false }), apiRouter.routes(), apiRouter.allowedMethods());
+router.use('/api', passport.authenticate('jwt', {session: false}), apiRouter.routes(), apiRouter.allowedMethods());
 
 module.exports = router;
