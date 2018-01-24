47e60b0a512f51d3c55c82041af7dda3a36c391b
CTzz <ctzz@protonmail.com>
2018-01-24 17:26:21 +0800

    Add testRouter

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,7 +1,9 @@
 const Router = require('koa-router');
 const todoRouter = require('./todo');
+const testRouter = require('./test');
 const router = new Router();
 
 router.use('/todo', todoRouter.routes(), todoRouter.allowedMethods());
+router.use('/test', testRouter.routes(), testRouter.allowedMethods());
 
 module.exports = router;
