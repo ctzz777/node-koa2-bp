2a021f725e59017b0f39c56f915cccce95ff73c4
CTzz <ctzz@protonmail.com>
2018-01-24 17:35:03 +0800

    Add logger

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,12 @@
+const logger = require('../utils/logger').getLogger('error');
+
 const handleError = () => {
   return async (ctx, next) => {
     try {
       await next();
     } catch (err) {
       ctx.status = err.status || 500;
+      logger.error(err.stack);
       // ctx.body = err.message;
       ctx.app.emit('error', err, ctx);
     }
